75c4701774c3989cd00f05414fe8b88fbfc6820c
hoodrich-fern <fernan17893@gmail.com>
2022-03-14 19:18:21 +0000

    char and s success

--- tests/firsttest.c
+++ tests/firsttest.c
@@ -4,6 +4,48 @@
 
 void bprintf(const char *format, va_list args)
 {
+	int state = 0;
+	while (*format)
+	{
+	if (state == 0)
+	{
+		if(*format == '%')
+		{
+			state = 1;
+		}
+		else
+		{
+			_putchar(*format);
+		}
+	}
+	else if (state == 1)
+		{
+		switch (*format)
+		{
+		case 'c':
+			{
+			char ch = va_arg(args, int);
+			_putchar(ch);
+			break;
+			}
+		case 's':
+			{
+			const char *s = va_arg(args, const char *);
+			for (; *s != '\0'; s++)
+				{
+				_putchar(*s);
+				}
+			break;
+			}
+		case 'd':
+			break;		
+		case 'i':
+			break;
+		}
+		state = 0;
+	}
+		format++;
+}
 }
 
 int _printf(const char *format, ...)
@@ -20,5 +62,12 @@ int _printf(const char *format, ...)
 int main()
 {
 
-	_printf("print a char %c");
+	_printf("print a char %c \n", 'H');
+	_printf("print a string %s \n", "hello world");
+	_printf("print an integer %d \n", 10);
+	_printf("print a decimal %d \n", 12);
+
+	return (0);
+
 }
+
