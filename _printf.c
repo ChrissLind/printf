fed23e7e32bdba54eb86e4818f46809bf886032c
hoodrich-fern <fernan17893@gmail.com>
2022-03-14 14:43:50 +0000

    printc update base

--- _printf.c
+++ _printf.c
@@ -1,1 +1,15 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
 
+
+int _printf(const char *format, ...)
+{
+
+	int i;
+	va_list arg;
+
+	if (format == NULL)
+		return (format - 1)
+
+}	
